478da91aa6187bc3b9e6de761796124023f42f0d
MonMon1997 <monmonmonmonmon11112@gmail.com>
2025-01-26 03:53:52 +0800

    5 commits - add screen shot

--- tests/assertion.spec.js
+++ tests/assertion.spec.js
@@ -60,7 +60,7 @@ test("Enable/Diable Demo", async ({ page }) => {
 });
 
 //Soft Assertion
-test.only("Soft Assertion Demo", async ({ page }) => {
+test("Soft Assertion Demo", async ({ page }) => {
   await page.goto("https://www.getty.edu/");
 
   await page.pause();
@@ -90,4 +90,79 @@ test.only("Soft Assertion Demo", async ({ page }) => {
   await expect(
     page.getByLabel("Search the Getty Website", { exact: true })
   ).toBeDisabled();
+});
+
+//Text matches/not matches value
+test("Text matches/not matches value Demo", async ({ page }) => {
+  await page.goto("https://www.getty.edu/");
+
+  await page.pause();
+
+  await page.getByText("Art & Archives").click();
+  //Text matches
+  await expect(page.getByText("Art & Archives")).toHaveText("Art & Archives");
+
+  //Text not matches
+  await expect(page.getByText("Art & Archives")).not.toHaveText("pepe");
+});
+
+//Element attribute
+test("Element attribute Demo", async ({ page }) => {
+  await page.goto("https://www.getty.edu/");
+
+  await page.pause();
+
+  //to have attribute
+  await expect
+    .soft(
+      page.getByText(
+        "Foundations and Arts Organizations Create $12M LA Arts Community Fire Relief Fund"
+      )
+    )
+    .toHaveAttribute("class", "a-rich-text a-rich-text--inline");
+  await page
+    .getByText(
+      "Foundations and Arts Organizations Create $12M LA Arts Community Fire Relief Fund"
+    )
+    .click();
+
+  const links = await page.getByRole("link", { name: "Hammer Museum" });
+  await links.click();
+
+  // TO Have Attribute
+  await expect(
+    page.getByAltText("Sepia-toned print of the American flag")
+  ).toHaveAttribute("loading", "eager");
+});
+
+//URL and Title
+test("URL and Title Demo", async ({ page }) => {
+  await page.goto("https://www.getty.edu/");
+
+  await page.pause();
+
+  const svgs = await page
+    .getByRole("contentinfo")
+    .locator("div")
+    .filter({ hasText: "Social Navigation See all" })
+    .getByRole("img")
+    .nth(4);
+
+  //Page
+  await expect(page).toHaveURL("https://www.getty.edu/");
+
+  //Title
+  await expect(page).toHaveTitle(
+    "Getty: Resources for Visual Art and Cultural Heritage"
+  );
+
+  await svgs.click();
+});
+
+//Screenshot
+test.only("Screenshot Demo", async ({ page }) => {
+  await page.goto("https://www.getty.edu/");
+
+  await page.pause();
+  await expect(page).toHaveScreenshot();
 });
